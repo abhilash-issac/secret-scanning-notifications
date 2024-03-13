ce7b4be5b32ddf77fd2ab7d850f542cb110eaa4f
Abhilash Issac <52700551+abhilash-issac@users.noreply.github.com>
2024-03-13 14:29:57 +0530

    Update summary.ts

--- src/services/summary.ts
+++ src/services/summary.ts
@@ -1,39 +1,32 @@
-import * as core from '@actions/core'
-import { SummaryTableRow } from '@actions/core/lib/summary'
-import { SecretScanningAlert } from '../types/common/main'
+import * as core from '@actions/core';
+import { SummaryTableRow } from '@actions/core/lib/summary';
+import { SecretScanningAlert } from '../types/common/main';
 
 export function addToSummary(title: string, alerts: SecretScanningAlert[]) {
-    const headers = ['Alert Number', 'Secret State', 'Secret Type', 'HTML URL', 'Org Owner', 'Repo Owner']
-    // Define the table rows
-    const rows = alerts.map(alert => {
-        const urlParts = alert.html_url.split('/')
-        const orgOwner = urlParts[3] // Assuming the org owner is the fourth element in the URL
-        const repoOwner = urlParts[4] // Assuming the repo owner is the fifth element in the URL
-        return [
-            alert.number.toString(),
-            alert.state,
-            alert.secret_type,
-            alert.html_url,
-            orgOwner,
-            repoOwner
-        ]
-    })
+  const headers = ['Alert Number', 'Secret State', 'Secret Type', 'HTML URL', 'Org Owner', 'Repo Owner'];
+  const rows = alerts.map(alert => [
+    alert.number.toString(),
+    alert.state,
+    alert.secret_type,
+    alert.html_url,
+    alert.org_owner, // Directly using the org_owner property
+    alert.repo_owner, // Directly using the repo_owner property
+  ]);
 
-    // Add the table to the Action summary
-    core.summary
-        .addHeading(title)
-        .addTable([
-            headers.map(header => ({ data: header, header: true })),
-            ...rows
-        ] as SummaryTableRow[])
-        .addBreak()
+  core.summary
+    .addHeading(title)
+    .addTable([
+      headers.map(header => ({ data: header, header: true })),
+      ...rows,
+    ] as SummaryTableRow[])
+    .addBreak();
 }
 
 export function writeSummary() {
-    core.summary.write()
-    core.info(`[✅] Action summary written`)
+  core.summary.write();
+  core.info(`[✅] Action summary written`);
 }
 
 export function getSummaryMarkdown() {
-    return core.summary.stringify()
+  return core.summary.stringify();
 }
